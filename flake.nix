ef6ca275c0c4797c727ec7b2a65cacc0b1295521
Different <github.shrubs231@passmail.net>
2024-08-28 19:22:25 +1000

    Catppuccin syntax highlighting

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
       flake = false;
     };
 
+    catppuccin-bat = {
+      url = "github:catppuccin/bat";
+      flake = false;
+    };
+
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -26,8 +31,9 @@
         pkgs,
         ...
       }: let
-        inherit (inputs) anemone;
+        inherit (inputs) anemone catppuccin-bat;
         themeName = (builtins.fromTOML (builtins.readFile "${anemone}/theme.toml")).name;
+        tmThemeName = "Catppuccin Mocha.tmTheme";
       in {
         packages = {
           different-name-blog = pkgs.stdenv.mkDerivation {
@@ -38,6 +44,9 @@
             configurePhase = ''
               mkdir -p "themes/${themeName}"
               cp -r ${anemone}/. "themes/${themeName}"
+
+              mkdir -p "highlight_themes"
+              cp "${catppuccin-bat}/themes/${tmThemeName}" "highlight_themes"
             '';
             buildPhase = "zola build";
             installPhase = "cp -r public $out";
@@ -50,12 +59,19 @@
           packages = with pkgs; [zola];
           shellHook = let
             themePath = "themes/${themeName}";
+            tmThemePath = "highlight_themes/${tmThemeName}";
           in ''
             mkdir -p themes
             if [ -L "${themePath}" ]; then
               rm "${themePath}"
             fi
             ln -sn "${anemone}" "${themePath}"
+
+            mkdir -p highlight_themes
+            if [ -L "${tmThemePath}" ]; then
+              rm "${tmThemePath}"
+            fi
+            ln -sn "${catppuccin-bat}/themes/${tmThemeName}" "${tmThemePath}"
           '';
         };
       };
